eb55476d76e64302b09549766dbf914a8f2f2fd6
Soheil Rashidi <contact@soheilrashidi.com>
2014-10-26 14:05:13 +0330

    Display is refreshed in 1s intervals only now.

--- controller/console.js
+++ controller/console.js
@@ -26,11 +26,10 @@ function ConsoleController(DataSource, argv) {
 ConsoleController.prototype.run = function() {
   var _this = this;
 
-  cls();
-
   _this.dataSource = new _this.DataSource(_this.argv);
 
   process.on("SIGINT", function() {
+    clearInterval(_this.timer);
     _this.dataSource.stop();
   });
 
@@ -39,93 +38,108 @@ ConsoleController.prototype.run = function() {
   });
 
   _this.dataSource.on("snapshot", function(snapshot) {
-    cls();
-
-    console.log(snapshot.host + " @ " + new Date(snapshot.time));
-    console.log();
-
-    var maxColumns = _.chain(snapshot.groups).max(function(group) { return group.column }).value().column;
-    var columnWidth = process.stdout.columns / maxColumns;
-
-    for (var i = 0; i <= maxColumns; i++) {
-      var columnX = Math.floor((i - 1) * columnWidth);
-      process.stdout.cursorTo(0, 2);
-
-      _.where(snapshot.groups, {column: i}).forEach(function(group) {
-        var groups = _.chain(snapshot.groups).where({column: i}).map(function(group) { return group.name }).value();
-        var counters = _.chain(snapshot.groups).where({column: i}).map(function(group) { return group.counters }).flatten().map(function(counter) { return counter.name }).value();
-        var lists = _.chain(snapshot.groups).where({column: i}).map(function(group) { return group.lists }).flatten().map(function(list) { return list.name }).value();
-        var listItems = _.chain(snapshot.groups).where({column: i}).map(function(group) { return group.lists }).flatten().map(function(list) { return list.items }).flatten().map(function(item) { return item.name }).value();
-        var listStatItems = _.chain(snapshot.groups).where({column: i}).map(function(group) { return group.lists }).flatten().map(function(list) { return list.stats }).flatten().compact().map(function(stat) { return stat.items }).flatten().map(function(item) { return item.name }).value();
-        var maxNameLength = _.max(_.union(groups, counters, lists, listItems, listStatItems), function(name) { return name.length }).length;
-
-        var indentation = maxNameLength - group.name.length + 2;
-        process.stdout.cursorTo(columnX);
-        console.log(repeat(" ", indentation) + group.name.cyan);
-
-        group.counters.forEach(function(counter) {
-          var indentation = maxNameLength - counter.name.length + 1;
-          var color = thresholdToColor(counter.threshold);
-          var value = numeral(counter.value).format(counter.format);
-          var stats = "";
-
-          if (counter.stats) {
-            counter.stats.forEach(function(stat) {
-              var indentation = maxNameLength - stat.name.length + 1;
-              var color = thresholdToColor(stat.threshold);
-              var value = numeral(stat.value).format(counter.format);
-
-              process.stdout.cursorTo(columnX);
-              stats += "  " + (stat.name + ": ").dim + color(value);
-            });
-          }
-
-          process.stdout.cursorTo(columnX);
-          console.log(repeat(" ", indentation) + counter.name + ": " + color(value) + stats);
-        });
-
-        group.lists.forEach(function(list) {
-          var indentation = maxNameLength - list.name.length + 2;
-
-          process.stdout.cursorTo(columnX);
-          console.log(repeat(" ", indentation) + list.name.cyan);
-
-          list.items.forEach(function(item) {
-            var indentation = maxNameLength - item.name.length + 1;
-            var value = numeral(item.value).format(list.format);
-
-            process.stdout.cursorTo(columnX);
-            console.log(repeat(" ", indentation) + item.name + ": " + value);
-          });
-
-          console.log();
-
-          if (list.stats) {
-            list.stats.forEach(function(stat) {
-              var indentation = maxNameLength - stat.name.length + 1;
-
-              process.stdout.cursorTo(columnX);
-              console.log(repeat(" ", indentation) + (stat.name + ": ").dim);
-
-              stat.items.forEach(function(item) {
-                var indentation = maxNameLength - item.name.length + 1;
-                var value = numeral(item.value).format(list.format);
-
-                process.stdout.cursorTo(columnX);
-                console.log(repeat(" ", indentation) + item.name + ": " + value);
-              });
-
-              console.log();
-            });
-          }
-        });
-
-        console.log();
-      });
-    };
+    _this.snapshot = snapshot;
   });
 
   _this.dataSource.start(_this.config);
+
+  _this.timer = setInterval((function(self) {
+    return function() {
+      self.displaySnapshot();
+    };
+  })(_this), 1000);
+};
+
+ConsoleController.prototype.displaySnapshot = function() {
+  var _this = this;
+
+  if (!_this.snapshot)
+    return;
+
+  cls();
+
+  console.log(_this.snapshot.host + " @ " + new Date(_this.snapshot.time));
+  console.log();
+
+  var maxColumns = _.chain(_this.snapshot.groups).max(function(group) { return group.column }).value().column;
+  var columnWidth = process.stdout.columns / maxColumns;
+
+  for (var i = 0; i <= maxColumns; i++) {
+    var columnX = Math.floor((i - 1) * columnWidth);
+    process.stdout.cursorTo(0, 2);
+
+    _.where(_this.snapshot.groups, {column: i}).forEach(function(group) {
+      var groups = _.chain(_this.snapshot.groups).where({column: i}).map(function(group) { return group.name }).value();
+      var counters = _.chain(_this.snapshot.groups).where({column: i}).map(function(group) { return group.counters }).flatten().map(function(counter) { return counter.name }).value();
+      var lists = _.chain(_this.snapshot.groups).where({column: i}).map(function(group) { return group.lists }).flatten().map(function(list) { return list.name }).value();
+      var listItems = _.chain(_this.snapshot.groups).where({column: i}).map(function(group) { return group.lists }).flatten().map(function(list) { return list.items }).flatten().map(function(item) { return item.name }).value();
+      var listStatItems = _.chain(_this.snapshot.groups).where({column: i}).map(function(group) { return group.lists }).flatten().map(function(list) { return list.stats }).flatten().compact().map(function(stat) { return stat.items }).flatten().map(function(item) { return item.name }).value();
+      var maxNameLength = _.max(_.union(groups, counters, lists, listItems, listStatItems), function(name) { return name.length }).length;
+
+      var indentation = maxNameLength - group.name.length + 2;
+      process.stdout.cursorTo(columnX);
+      console.log(repeat(" ", indentation) + group.name.cyan);
+
+      group.counters.forEach(function(counter) {
+        var indentation = maxNameLength - counter.name.length + 1;
+        var color = thresholdToColor(counter.threshold);
+        var value = numeral(counter.value).format(counter.format);
+        var stats = "";
+
+        if (counter.stats) {
+          counter.stats.forEach(function(stat) {
+            var indentation = maxNameLength - stat.name.length + 1;
+            var color = thresholdToColor(stat.threshold);
+            var value = numeral(stat.value).format(counter.format);
+
+            process.stdout.cursorTo(columnX);
+            stats += "  " + (stat.name + ": ").dim + color(value);
+          });
+        }
+
+        process.stdout.cursorTo(columnX);
+        console.log(repeat(" ", indentation) + counter.name + ": " + color(value) + stats);
+      });
+
+      group.lists.forEach(function(list) {
+        var indentation = maxNameLength - list.name.length + 2;
+
+        process.stdout.cursorTo(columnX);
+        console.log(repeat(" ", indentation) + list.name.cyan);
+
+        list.items.forEach(function(item) {
+          var indentation = maxNameLength - item.name.length + 1;
+          var value = numeral(item.value).format(list.format);
+
+          process.stdout.cursorTo(columnX);
+          console.log(repeat(" ", indentation) + item.name + ": " + value);
+        });
+
+        console.log();
+
+        if (list.stats) {
+          list.stats.forEach(function(stat) {
+            var indentation = maxNameLength - stat.name.length + 1;
+
+            process.stdout.cursorTo(columnX);
+            console.log(repeat(" ", indentation) + (stat.name + ": ").dim);
+
+            stat.items.forEach(function(item) {
+              var indentation = maxNameLength - item.name.length + 1;
+              var value = numeral(item.value).format(list.format);
+
+              process.stdout.cursorTo(columnX);
+              console.log(repeat(" ", indentation) + item.name + ": " + value);
+            });
+
+            console.log();
+          });
+        }
+      });
+
+      console.log();
+    });
+  };
 };
 
 module.exports = ConsoleController;
